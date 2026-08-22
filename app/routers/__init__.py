6826d4439c6fe23c128dc68fb4f992420644088e
minhao23 <heminhao120@gmail.com>
2026-08-22 10:16:27 +0800

    feat: add driver router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,3 @@
-from . import apigateway_router, ghost_chains, showdown_router
+from . import apigateway_router, ghost_chains, showdown_router, driver_router
 
-routers = [apigateway_router.router, showdown_router.router, ghost_chains.router]
+routers = [apigateway_router.router, showdown_router.router, ghost_chains.router, driver_router.router]
