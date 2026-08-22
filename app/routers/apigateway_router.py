cfda9e4c614cdcf49073290e3ebc4d7aa7a625d2
minhao23 <heminhao120@gmail.com>
2026-08-22 14:02:44 +0800

    huh

--- app/routers/apigateway_router.py
+++ app/routers/apigateway_router.py
@@ -93,7 +93,7 @@ def _compute_slo(
         window = [
             hb
             for hb in heartbeats
-            if hb.timestamp >= slo_query.since
+            if hb.timestamp == slo_query.since
         ]
 
     if not window:
