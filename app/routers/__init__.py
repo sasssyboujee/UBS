7e26b422bc1d00b54870fa554c950172f4782329
minhao23 <heminhao120@gmail.com>
2026-08-22 10:26:43 +0800

    feat: use kancheong router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,3 @@
-from . import apigateway_router, ghost_chains, showdown_router, driver_router
+from . import apigateway_router, ghost_chains, showdown_router, kan_cheong
 
-routers = [apigateway_router.router, showdown_router.router, ghost_chains.router, driver_router.router]
+routers = [apigateway_router.router, showdown_router.router, ghost_chains.router, kan_cheong.router]
